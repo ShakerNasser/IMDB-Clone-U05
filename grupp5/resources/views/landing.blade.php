b012975789c2fb04df12ef57787bc1f7e8c1e673
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2024-02-16 16:32:32 +0000

    Laravel mix installed

--- grupp5/resources/views/landing.blade.php
+++ grupp5/resources/views/landing.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ asset('nav_footer/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
     <title>Home</title>
@@ -14,6 +14,7 @@
     @include('layout.navbar')
     <section>
         <h1>Top movies</h1>
+        <button class="button-test">BUTTON</button>
     </section>
 </body>
 @include('layout.footer')
