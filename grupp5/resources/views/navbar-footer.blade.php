56d48cdad5ed47d456716e457b8d86b5f65cd454
ArianAFK <arianfekri@hotmail.com>
2024-02-23 13:46:54 +0000

    navbar progress

--- grupp5/resources/views/navbar-footer.blade.php
+++ grupp5/resources/views/navbar-footer.blade.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-    @include('meta')
+@include('meta')
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body class="bg-fuchsia-500
-">
-    <h1 class="text-blue-500">Hello</h1>
 
+<body class="bg-background">
+    <nav class="fixed md:flex flex flex-row h-fit w-full py-2 bg-nav-color md:items-center md:justify-between">
+        <img src="{{ asset('images/Logo.png')}}" alt="logo" class="size-12 ml-4 inline cursor-pointer">
+        <div class="md:flex md:items-center z-[-1] md:z-auto md:static bg-green-950 w-full">
+            <form action="" method="GET">
+                <input type="text" placeholder="Search movie.." name="query" class="bg-search-field-color border-none rounded-l-lg"><button type="submit" class="bg-search-btn-color p-2 text-text border-none rounded-r-lg">Search</button></input>
+            </form>
+            @if (Route::has('login'))
+            <div class="mr-4 ">
+                @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-text hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}" class="font-semibold text-text hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-text hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif
+        </div>
+    </nav>
 </body>
+
 </html>
--- grupp5/tailwind.config.js
+++ grupp5/tailwind.config.js
@@ -7,7 +7,9 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
@@ -18,20 +20,20 @@ export default {
             colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
-                    'nav-color': '#820000',
-                    'search-btn-color': '#C23C3C',
-                    'search-field-color': '#591C1C',
-                    'signin-btn-color': 'rgba(75, 0, 15, 0.6)',
-                    'button-txt': '#628CF8',
-                    'banner-color': '#820000',
-                    'delete-button-color': '#CC0028',
-                    'button-background': '#3A3A3A',
-                    'form-background-color': '#2E2E2E',
-                    'card-background': '#222222',
-                    'text':'#F2EDEE',
-                    'background': '#100809',
-      },
-           
+                'nav-color': '#820000',
+                'search-btn-color': '#C23C3C',
+                'search-field-color': '#591C1C',
+                'signin-btn-color': 'rgba(75, 0, 15, 0.6)',
+                'button-txt': '#628CF8',
+                'banner-color': '#820000',
+                'delete-button-color': '#CC0028',
+                'button-background': '#3A3A3A',
+                'form-background-color': '#2E2E2E',
+                'card-background': '#222222',
+                'text': '#F2EDEE',
+                'background': '#100809',
+            },
+
         },
     },
 
