4812ab2bb8e9057e9dfc5658c707662ad4a2aad8
ShakerNasser <shaker.nasser@chasacademy.se>
2024-02-25 15:42:02 +0000

    trailer och Ombed

--- grupp5/resources/views/trailer.blade.php
+++ grupp5/resources/views/trailer.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 @include('meta')
-
+@include('navbar')
 <body class="bg-background">
-<h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">Movie Review</span></h1>
 
-    <div class="container">
-       
+    <div class="container mx-auto">
+        <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">Movie trailer</span></h1>
+
         @foreach ($movies as $movie)
-        <h1 class=" text-text">Trailer: {{ $movie->title }} </h1>
-        <div>
-            <h2 class=" text-text">{{ $movie->title }}</h2>
-            <h2 class=" text-text">{{ $movie->genre }}</h2>
-            {!! $movie->embedCode->html() !!}
+        <div class="flex flex-col items-center mb-10">
+            <h2 class="text-text">{{ $movie->title }}</h2>
+            <h2 class="text-text">{{ $movie->genre }}</h2>
+
+            <div>
+                {!! $movie->embedCode->html() !!}
+            </div>
         </div>
         @endforeach
-
-
     </div>
+    @include('footer')
 
 </body>
 
