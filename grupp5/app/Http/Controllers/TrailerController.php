b099e712e4cb472e1da7392614f1b4c38b02c19b
Shaker Nasser <144438906+ShakerNasser@users.noreply.github.com>
2024-02-25 16:49:32 +0100

    Revert "Revert "Trailer""

--- grupp5/app/Http/Controllers/TrailerController.php
+++ grupp5/app/Http/Controllers/TrailerController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Cohensive\OEmbed\Facades\OEmbed;
 use App\Models\Movie;
 
 class TrailerController extends Controller
 {
     public function index()
-{
-    $movies = Movie::all();
+    {
+        // Fetch all movies from the database
+        $movies = Movie::all();
 
-    return view('trailers.index', compact('movies'));
-}
+        // Loop through each movie and fetch the embed code for the YouTube video
+        foreach ($movies as $movie) {
+            // Fetch the embed code for the YouTube video
+            $embedCode = OEmbed::get($movie->trailer_url);
 
+            // Store the embed code in the $movie object
+            $movie->embedCode = $embedCode;
+        }
+
+        // Pass the movies to the view
+        return view('trailer', compact('movies'));
+    }
 }
