3df8964030c5fb21ae565374a1644a85481986ed
ShakerNasser <shaker.nasser@chasacademy.se>
2024-02-27 15:00:34 +0000

    reviews admin

--- grupp5/resources/views/movies.blade.php
+++ grupp5/resources/views/movies.blade.php
@@ -6,19 +6,57 @@
 <title>{{ $movie->title }}</title>
 
 <body class="bg-background">
-<section class="px-10 py-10">
-    <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">About: {{ $movie->title }}</span></h1>
-  </section>
+    <section class="px-10 py-10">
+        <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">About: {{ $movie->title }}</span></h1>
+    </section>
 
-    <div>
+    <div class="flex px-10 pt-4 pb-72 text-text">
         <p> Genre:{{ $movie->genre }}</p>
-        <p>{{ $movie->description }}</p>
-
-        <p><a href="{{ route('trailers.index', ['movie_id' => $movie->id]) }}" class="btn py-2 px-4 rounded text-button-txt bg-button-background w-100 hover:bg-red-800">Go to trailer</a></p>
-
+        <p> Description {{ $movie->description }}</p>
+        <div>
+            <a href="{{ route('trailers.index', ['movie_id' => $movie->id]) }}" class="btn py-2 px-4 rounded text-button-txt bg-button-background w-100 hover:bg-red-800">Go to trailer</a>
+        </div>
     </div>
 
-    @include('footer')
+    <div class="flex px-10 pt-4 pb-72 text-text">
+        <div class="px-10 py-10">
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">Review</th>
+                        @if (auth()->check() && auth()->user()->role == 0)
+                        <th class="px-4 py-2">Actions</th>
+                        @endif
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($reviews->where('movie_id', $movie->id)->reverse() as $review)
+                    <tr>
+                        <td class="px-4 py-2">{{ $review->review }}</td>
+                        <td class="px-4 py-2">
+                            <form action="{{ route('review.approve', $review->id) }}" method="POST">
+                                @csrf
+                                @if (auth()->check() && auth()->user()->role == 0)
+                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Approve</button>
+                                @endif
+                            </form>
+                        </td>
+
+                        <td>
+                            <form action="{{ route('review.delete', $review->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                @if (auth()->check() && auth()->user()->role == 0)
+                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                @endif
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
--- grupp5/resources/views/review.blade.php
+++ grupp5/resources/views/review.blade.php
@@ -2,32 +2,26 @@
 <html lang="en">
 
 @include('meta')
+@include('navbar')
 
-<body class="bg-background flex flex-col justify-center items-center h-screen">
-    <card class="relative flex max-w-[16rem] flex-col overflow-hidden rounded-xl bg-card-background bg-clip-border text-gray-700 shadow-md">
-        <div class="min-h-[60%] relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border">
-            <img src="{{ $movie->image }}" alt="Movie Image" class="h-full object-cover" />
-        </div>
-        <div class="p-3">
-            <h1 class="text-2xl antialiased font-semibold leading-snug tracking-normal text-center text-text">
-                {{ $movie->title }}
-            </h1>
-        </div>
-    </card>
-
+<body class="bg-background">
     <section class="px-10 py-10">
         <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">Movie Review</span></h1>
-        @if(session('status'))
-        <div class="alert alert-success" style="color: green;">
-            {{ session('status') }}
-        </div>
-        @endif
+
+    </section>
+
+    {{-- Form to add a review --}}
+
+    <section class="px-10 py-10">
+        <h2 class="text-3xl font-bold mb-5 text-text">{{ $movie->title }}</h2>
+
+ 
         <div class="mx-auto max-w-screen-md mt-4">
             <form action="{{ route('reviews.store', ['movie_id' => $movie_id]) }}" method="POST" class="border border-gray-300 rounded-lg p-4">
                 @csrf
                 <p>
                     <label for="review" class="block text-text mt-3">Leave a review:</label>
-                    <input maxlength="50" size="30" required placeholder="Review" name="review" class="form-input w-full bg-gray-100 border border-gray-300 rounded p-2" />
+                    <input maxlength="100" size="40" required placeholder="Inspire others..." name="review" class="form-input w-full bg-gray-100 border border-gray-300 rounded p-2" />
                 </p>
                 <div class="text-center">
                     <button type="submit" name="submit" class=" text-button-txt bg-button-background font-bold py-2 px-4 rounded mt-6 text-center">Add review</button>
@@ -35,15 +29,15 @@
             </form>
         </div>
     </section>
-    @foreach ($reviews->where('movie_id', $movie->id)->reverse() as $review)
-    <card class="">
-        <div class="">
-            <p class="text-text">
-                {{ $review->review}}
-            </p>
-    </card>
-    @endforeach
 
+    <div class="text-text">
+    @if(session('status'))
+        <div class="alert alert-success" style="color: green;">
+            {{ session('status') }}
+        </div>
+        @endif
+        </div>
+    @include('footer')
 
 </body>
 
