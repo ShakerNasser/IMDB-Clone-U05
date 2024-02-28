8b87b2744a2f9ff0d7108eaee8143cb2e35170da
Heval Demirel <hevalzzz@hotmail.com>
2024-02-28 13:10:07 +0000

    Config

--- grupp5/resources/views/aboutus.blade.php
+++ grupp5/resources/views/aboutus.blade.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 @include('meta')
 @include('navbar')
 
-<body class="bg-background">
-<section class="px-10 py-10">
-        <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">About us</span></h1>
+<body class="bg-background relative">
+
+  <div class="absolute inset-x-0 top-1/4 transform -translate-y-1/2 text-center w-full">
+    <section class="px-10 py-10">
+      <h1 class="text-5xl font-bold mb-5 text-text"><span class="underline">About us</span></h1>
     </section>
-    <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
-        <main class="mt-8">
+  </div>
 
-            <article class="prose lg:prose-lg xl:prose-xl text-text">
-                <p>MovieHub is a passionate community of film lovers who believe in the power of stories and how they can bring people together. We are convinced that movies are not just entertainment, but also a source of inspiration, reflection, and change.</p>
-                <p>Our platform is not just a database of movies and actors; it's a community where people can share their thoughts, ideas, and feelings about movies. We believe in creating an inclusive and welcoming environment where everyone can feel at home, whether they are beginners or experienced film enthusiasts.</p>
-                <p>We are proud to be a part of the global film community and look forward to continuing to grow and evolve together with our users. Thank you for being a part of MovieHub!</p>
-            </article>
+  <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 mt-28">
 
-        </main>
-    </div>
-    @include('footer')
+      <article class="prose lg:prose-lg xl:prose-xl text-text">
+        <p>MovieHub is a passionate community of film lovers who believe in the power of stories and how they can bring people together. We are convinced that movies are not just entertainment, but also a source of inspiration, reflection, and change.</p>
+        <p>Our platform is not just a database of movies and actors; it's a community where people can share their thoughts, ideas, and feelings about movies. We believe in creating an inclusive and welcoming environment where everyone can feel at home, whether they are beginners or experienced film enthusiasts.</p>
+        <p>We are proud to be a part of the global film community and look forward to continuing to grow and evolve together with our users. Thank you for being a part of MovieHub!</p>
+      </article>
+
+  </div>
+@include('footer')
 </body>
 
 </html>
+
--- grupp5/resources/views/search.blade.php
+++ grupp5/resources/views/search.blade.php
@@ -5,18 +5,16 @@
 
 <body class="bg-background">
 
-    <div class="container mx-auto flex justify-center items-center min-h-screen">
+    <div class="container  mx-auto flex justify-center items-center min-h-screen">
         <div class="bg-custom-color rounded-lg overflow-hidden shadow-md w-full md:max-w-lg">
             @if($movies->count() > 0)
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
                 @foreach($movies as $movie)
-                <div class="bg-white rounded-lg overflow-hidden shadow-md transition-transform transform hover:scale-105">
                     <div class="p-8">
-                        <h2 class="text-2xl font-semibold text-gray-800 mb-2">{{ $movie->title }}</h2>
-                        <p class="text-gray-600 mb-4">{{ $movie->description }}</p>
-                        <p class="text-gray-600 font-semibold">{{ $movie->genre }}</p>
+                        <h2 class="text-2xl font-semibold text-text mb-2">{{ $movie->title }}</h2>
+                        <p class="text-text mb-4">{{ $movie->description }}</p>
+                        <p class="text-text font-semibold">{{ $movie->genre }}</p>
                     </div>
-                </div>
                 @endforeach
             </div>
 
